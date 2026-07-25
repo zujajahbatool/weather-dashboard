a507d57f1ca6d8f5be71f8e6aacf0af286770f9f
zujajahbatool <batoolzujajah@gmail.com>
2026-07-25 17:30:46 +0500

    made sure that the component fetches real time data and handles errors

--- src/components/FiveDayForecast/FiveDayForecast.jsx
+++ src/components/FiveDayForecast/FiveDayForecast.jsx
@@ -1,18 +1,9 @@
 import DayForecastCard from '../DayForecastCard/DayForecastCard';
-import rainyIcon from '../../assets/icons/rainy_icon.png';
-import partlySunnyIcon from '../../assets/icons/partlysunny_icon.png';
-import stormIcon from '../../assets/icons/stormy_icon.png';
 import './FiveDayForecast.css';
 
-const forecastData = [
-  { id: 'today', day: 'Today', icon: rainyIcon, iconAlt: 'Heavy rain', temp: 28 },
-  { id: 'mon', day: 'Mon', icon: partlySunnyIcon, iconAlt: 'Partly sunny', temp: 31 },
-  { id: 'tue', day: 'Tue', icon: rainyIcon, iconAlt: 'Heavy rain', temp: 27 },
-  { id: 'wed', day: 'Wed', icon: stormIcon, iconAlt: 'Storm', temp: 29 },
-  { id: 'thu', day: 'Thu', icon: partlySunnyIcon, iconAlt: 'Partly sunny', temp: 32 },
-];
+export default function FiveDayForecast({ forecastData = [], unit = "C" }) {
+  if (!forecastData || forecastData.length === 0) return null;
 
-function FiveDayForecast() {
   return (
     <div className="fdf-card">
       <h2 className="fdf-title">5 Day Forecast</h2>
@@ -22,13 +13,13 @@ function FiveDayForecast() {
             key={item.id}
             day={item.day}
             icon={item.icon}
-            iconAlt={item.iconAlt}
-            temp={item.temp}
+            iconAlt={item.condition}
+            tempMax={item.tempMax}
+            tempMin={item.tempMin}
+            unit={unit}
           />
         ))}
       </div>
     </div>
   );
 }
-
-export default FiveDayForecast;
